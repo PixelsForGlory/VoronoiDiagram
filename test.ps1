f2140a18d501daa4ce6d664bb7cbceffcceaa815
afuzzyllama <karcieri@gmail.com>
2016-06-12 12:02:13 -0400

    Cleaning up build process

--- test.ps1
+++ test.ps1
@@ -1,3 +1,4 @@
 # Run unit test
 Copy-Item $env:APPVEYOR_BUILD_FOLDER\VoronoiDiagram\bin\Release\VoronoiDiagram.* -Destination $env:APPVEYOR_BUILD_FOLDER\VoronoiDiagramTest\bin\Release\ -Force
 vstest.console $env:APPVEYOR_BUILD_FOLDER\VoronoiDiagramTest\bin\Release\VoronoiDiagramTest.dll /Settings:$env:APPVEYOR_BUILD_FOLDER\VoronoiDiagramTest\test.runsettings /logger:Appveyor
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
