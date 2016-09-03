32d0e90490975de8ea91999e976339bce5f1f473
afuzzyllama <karcieri@gmail.com>
2016-09-03 11:48:15 -0400

    Fixed build scripts

--- packageBuild.ps1
+++ packageBuild.ps1
@@ -1,6 +1,6 @@
 $destinationFolder = "$env:STAGING_DIR\ReleaseContents\Plugins\"
 if (!(Test-Path -path $destinationFolder)) {New-Item $destinationFolder -Type Directory}
-Copy-Item $env:APPVEYOR_BUILD_FOLDER\VoronoiDiagram\bin\Release\VoronoiDiagram.dll -Destination $destinationFolder -Force 
+Copy-Item $env:APPVEYOR_BUILD_FOLDER\VoronoiDiagram\bin\Release\PixelsForGlory.ComputationalSystem.VoronoiDiagram.dll -Destination $destinationFolder -Force 
 
 $destinationFolder = "$env:STAGING_DIR\ReleaseContents\"
 if (!(Test-Path -path $destinationFolder)) {New-Item $destinationFolder -Type Directory}
